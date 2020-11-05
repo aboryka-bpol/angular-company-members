bd386d1051abc6d228f78a74299a907cd999feab
aboryka-bpol <aleksandra.boryka@bpol.net>
2020-11-05 17:06:25 +0100

    Update response type in resolver

--- src/app/services/resolvers/team-members-resolver.service.ts
+++ src/app/services/resolvers/team-members-resolver.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { IEmployeeData } from 'src/app/interfaces/employee';
+import { Team } from 'src/app/models/Team.model';
 import { EmployeesService } from '../employees.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TeamMembersResolverService implements Resolve<IEmployeeData[]> {
+export class TeamMembersResolverService implements Resolve<Team[]> {
 
   constructor(private employeeService: EmployeesService) { }
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IEmployeeData[]> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Team[]> {
     return this.employeeService.getEmployees();
   }
 }
